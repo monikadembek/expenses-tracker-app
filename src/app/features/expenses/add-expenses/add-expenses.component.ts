96a0ebe307452dfac424d51bfab7d2bdc5f81a4e
Monika Dembek <monika.dembek@gmail.com>
2021-07-24 15:01:03 +0200

    add validation for add expense form

--- src/app/features/expenses/add-expenses/add-expenses.component.ts
+++ src/app/features/expenses/add-expenses/add-expenses.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormGroupDirective, AbstractControl } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Expense } from '../models/Expense';
@@ -27,6 +27,38 @@ export class AddExpensesComponent implements OnInit, OnDestroy {
 
   @ViewChild(FormGroupDirective) formDirective: FormGroupDirective;
 
+  get expenseTitle(): AbstractControl {
+    return this.addExpenseForm.get('expenseTitle');
+  }
+
+  get expenseValue(): AbstractControl {
+    return this.addExpenseForm.get('value');
+  }
+
+  get expenseDate(): AbstractControl {
+    return this.addExpenseForm.get('date');
+  }
+
+  get expenseCategory(): AbstractControl {
+    return this.addExpenseForm.get('category');
+  }
+
+  get isTitleValid(): boolean {
+    return this.expenseTitle.dirty && this.expenseTitle.valid;
+  }
+
+  get isValueValid(): boolean {
+    return this.expenseValue.dirty && this.expenseValue.valid;
+  }
+
+  get isDateValid(): boolean {
+    return this.expenseDate.dirty && this.expenseDate.valid;
+  }
+
+  get isCategoryValid(): boolean {
+    return this.expenseCategory.dirty && this.expenseCategory.valid;
+  }
+
   constructor(private expensesService: ExpensesService,
               private fb: FormBuilder) { }
 
@@ -36,7 +68,7 @@ export class AddExpensesComponent implements OnInit, OnDestroy {
 
   private buildForm(): void {
     this.addExpenseForm = this.fb.group({
-      expenseTitle: ['', [Validators.required, Validators.minLength(2)]],
+      expenseTitle: ['', [Validators.required]],
       value: [0, [Validators.required, Validators.min(0)]],
       date: [this.initialDate, [Validators.required]],
       category: ['other', [Validators.required]]
@@ -48,7 +80,7 @@ export class AddExpensesComponent implements OnInit, OnDestroy {
     if (form.dirty && form.valid) {
       const expense: Expense = {
         ...form.value,
-        date: new Date(form.value.date)
+        // date: new Date(form.value.date)
       };
       this.expensesService.addExpense(expense)
         .pipe(takeUntil(this.unSubscribe$))
@@ -59,6 +91,38 @@ export class AddExpensesComponent implements OnInit, OnDestroy {
           }
         });
     }
+  }
+
+  getErrorMsg(control: string): string {
+    let errorMsg = '';
+    switch (control) {
+      case 'expenseTitle':
+        if (!this.isTitleValid) {
+          errorMsg = 'Please enter title of expense';
+        }
+        break;
+      case 'value':
+        if (!this.isValueValid && this.expenseValue.errors?.required) {
+          errorMsg = 'Please enter value of expense';
+        }
+        if (!this.isValueValid && this.expenseValue.errors?.min) {
+          errorMsg = 'Value can\'t be less than 0';
+        }
+        break;
+      case 'date':
+        if (!this.isDateValid) {
+          errorMsg = 'Please enter date';
+        }
+        break;
+      case 'category':
+        if (!this.isCategoryValid) {
+          errorMsg = 'Please select category';
+        }
+        break;
+      default:
+        errorMsg = '';
+    }
+    return errorMsg;
   }
 
   ngOnDestroy(): void {
